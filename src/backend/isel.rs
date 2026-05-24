b081865c08e0e6ca7c48199bee6c9d7900836549
Mythrax <mythrax@mythrax-rs.org>
2026-05-24 14:29:56 +1000

    Collapse sized emit helpers behind single bits arg

--- src/backend/isel.rs
+++ src/backend/isel.rs
@@ -129,52 +129,33 @@ pub fn emit_armlet(
             asm.mov(qword_ptr(CTX_REG + off + 8), 0i32)?;
         }
 
-        Op::Add32 => emit_binop_32(asm, alloc, a, dst_vr, BinKind::Add)?,
-        Op::Add64 => emit_binop_64(asm, alloc, a, dst_vr, BinKind::Add)?,
-        Op::Sub32 => emit_binop_32(asm, alloc, a, dst_vr, BinKind::Sub)?,
-        Op::Sub64 => emit_binop_64(asm, alloc, a, dst_vr, BinKind::Sub)?,
-        Op::And32 => emit_binop_32(asm, alloc, a, dst_vr, BinKind::And)?,
-        Op::And64 => emit_binop_64(asm, alloc, a, dst_vr, BinKind::And)?,
-        Op::Or32  => emit_binop_32(asm, alloc, a, dst_vr, BinKind::Or)?,
-        Op::Or64  => emit_binop_64(asm, alloc, a, dst_vr, BinKind::Or)?,
-        Op::Eor32 => emit_binop_32(asm, alloc, a, dst_vr, BinKind::Xor)?,
-        Op::Eor64 => emit_binop_64(asm, alloc, a, dst_vr, BinKind::Xor)?,
-        Op::Mul32 => emit_binop_32(asm, alloc, a, dst_vr, BinKind::Imul)?,
-        Op::Mul64 => emit_binop_64(asm, alloc, a, dst_vr, BinKind::Imul)?,
+        Op::Add32 | Op::Add64 => emit_binop(asm, alloc, a, dst_vr, BinKind::Add,  a.op.size_bits())?,
+        Op::Sub32 | Op::Sub64 => emit_binop(asm, alloc, a, dst_vr, BinKind::Sub,  a.op.size_bits())?,
+        Op::And32 | Op::And64 => emit_binop(asm, alloc, a, dst_vr, BinKind::And,  a.op.size_bits())?,
+        Op::Or32  | Op::Or64  => emit_binop(asm, alloc, a, dst_vr, BinKind::Or,   a.op.size_bits())?,
+        Op::Eor32 | Op::Eor64 => emit_binop(asm, alloc, a, dst_vr, BinKind::Xor,  a.op.size_bits())?,
+        Op::Mul32 | Op::Mul64 => emit_binop(asm, alloc, a, dst_vr, BinKind::Imul, a.op.size_bits())?,
 
-        Op::Adc32 => emit_adc_sbc(asm, alloc, a, dst_vr, false, false)?,
-        Op::Adc64 => emit_adc_sbc(asm, alloc, a, dst_vr, false, true)?,
-        Op::Sbc32 => emit_adc_sbc(asm, alloc, a, dst_vr, true,  false)?,
-        Op::Sbc64 => emit_adc_sbc(asm, alloc, a, dst_vr, true,  true)?,
+        Op::Adc32 | Op::Adc64 => emit_adc_sbc(asm, alloc, a, dst_vr, false, a.op.size_bits())?,
+        Op::Sbc32 | Op::Sbc64 => emit_adc_sbc(asm, alloc, a, dst_vr, true,  a.op.size_bits())?,
 
-        Op::UDiv32 => emit_div(asm, alloc, a, dst_vr, false, false)?,
-        Op::UDiv64 => emit_div(asm, alloc, a, dst_vr, false, true)?,
-        Op::SDiv32 => emit_div(asm, alloc, a, dst_vr, true,  false)?,
-        Op::SDiv64 => emit_div(asm, alloc, a, dst_vr, true,  true)?,
+        Op::UDiv32 | Op::UDiv64 => emit_div(asm, alloc, a, dst_vr, false, a.op.size_bits())?,
+        Op::SDiv32 | Op::SDiv64 => emit_div(asm, alloc, a, dst_vr, true,  a.op.size_bits())?,
 
-        Op::Clz32 => emit_clz(asm, alloc, a, dst_vr, false)?,
-        Op::Clz64 => emit_clz(asm, alloc, a, dst_vr, true)?,
-        Op::Cls32 => emit_cls(asm, alloc, a, dst_vr, false)?,
-        Op::Cls64 => emit_cls(asm, alloc, a, dst_vr, true)?,
-        Op::Rbit32 => emit_rbit(asm, alloc, a, dst_vr, false)?,
-        Op::Rbit64 => emit_rbit(asm, alloc, a, dst_vr, true)?,
-        Op::Rev16  => emit_rev16(asm, alloc, a, dst_vr, a.ty == Ty::U64)?,
+        Op::Clz32  | Op::Clz64  => emit_clz(asm, alloc, a, dst_vr, a.op.size_bits())?,
+        Op::Cls32  | Op::Cls64  => emit_cls(asm, alloc, a, dst_vr, a.op.size_bits())?,
+        Op::Rbit32 | Op::Rbit64 => emit_rbit(asm, alloc, a, dst_vr, a.op.size_bits())?,
+        Op::Rev16  => emit_rev16(asm, alloc, a, dst_vr, if a.ty == Ty::U64 { 64 } else { 32 })?,
         Op::Rev32  => emit_rev32_within64(asm, alloc, a, dst_vr)?,
-        Op::Rev64  => emit_bswap(asm, alloc, a, dst_vr, a.ty == Ty::U64)?,
+        Op::Rev64  => emit_bswap(asm, alloc, a, dst_vr, if a.ty == Ty::U64 { 64 } else { 32 })?,
 
-        Op::Lsl32 => emit_shift_32(asm, alloc, a, dst_vr, ShiftKind::Lsl)?,
-        Op::Lsl64 => emit_shift_64(asm, alloc, a, dst_vr, ShiftKind::Lsl)?,
-        Op::Lsr32 => emit_shift_32(asm, alloc, a, dst_vr, ShiftKind::Lsr)?,
-        Op::Lsr64 => emit_shift_64(asm, alloc, a, dst_vr, ShiftKind::Lsr)?,
-        Op::Asr32 => emit_shift_32(asm, alloc, a, dst_vr, ShiftKind::Asr)?,
-        Op::Asr64 => emit_shift_64(asm, alloc, a, dst_vr, ShiftKind::Asr)?,
-        Op::Ror32 => emit_shift_32(asm, alloc, a, dst_vr, ShiftKind::Ror)?,
-        Op::Ror64 => emit_shift_64(asm, alloc, a, dst_vr, ShiftKind::Ror)?,
+        Op::Lsl32 | Op::Lsl64 => emit_shift(asm, alloc, a, dst_vr, ShiftKind::Lsl, a.op.size_bits())?,
+        Op::Lsr32 | Op::Lsr64 => emit_shift(asm, alloc, a, dst_vr, ShiftKind::Lsr, a.op.size_bits())?,
+        Op::Asr32 | Op::Asr64 => emit_shift(asm, alloc, a, dst_vr, ShiftKind::Asr, a.op.size_bits())?,
+        Op::Ror32 | Op::Ror64 => emit_shift(asm, alloc, a, dst_vr, ShiftKind::Ror, a.op.size_bits())?,
 
-        Op::Not32 => emit_unop_32(asm, alloc, a, dst_vr, UnopKind::Not)?,
-        Op::Not64 => emit_unop_64(asm, alloc, a, dst_vr, UnopKind::Not)?,
-        Op::Neg32 => emit_unop_32(asm, alloc, a, dst_vr, UnopKind::Neg)?,
-        Op::Neg64 => emit_unop_64(asm, alloc, a, dst_vr, UnopKind::Neg)?,
+        Op::Not32 | Op::Not64 => emit_unop(asm, alloc, a, dst_vr, UnopKind::Not, a.op.size_bits())?,
+        Op::Neg32 | Op::Neg64 => emit_unop(asm, alloc, a, dst_vr, UnopKind::Neg, a.op.size_bits())?,
 
         Op::AddsFlags32 | Op::AddsFlags64 | Op::SubsFlags32 | Op::SubsFlags64 => {
             emit_flagged_addsub(asm, alloc, a, dst_vr)?;
@@ -192,14 +173,10 @@ pub fn emit_armlet(
 
         Op::Csel32 | Op::Csel64 => emit_csel(asm, alloc, a, dst_vr)?,
 
-        Op::Fadd32 => emit_fbinop_s(asm, alloc, a, dst_vr, FpBinKind::Add)?,
-        Op::Fadd64 => emit_fbinop_d(asm, alloc, a, dst_vr, FpBinKind::Add)?,
-        Op::Fsub32 => emit_fbinop_s(asm, alloc, a, dst_vr, FpBinKind::Sub)?,
-        Op::Fsub64 => emit_fbinop_d(asm, alloc, a, dst_vr, FpBinKind::Sub)?,
-        Op::Fmul32 => emit_fbinop_s(asm, alloc, a, dst_vr, FpBinKind::Mul)?,
-        Op::Fmul64 => emit_fbinop_d(asm, alloc, a, dst_vr, FpBinKind::Mul)?,
-        Op::Fdiv32 => emit_fbinop_s(asm, alloc, a, dst_vr, FpBinKind::Div)?,
-        Op::Fdiv64 => emit_fbinop_d(asm, alloc, a, dst_vr, FpBinKind::Div)?,
+        Op::Fadd32 | Op::Fadd64 => emit_fbinop(asm, alloc, a, dst_vr, FpBinKind::Add, a.op.size_bits())?,
+        Op::Fsub32 | Op::Fsub64 => emit_fbinop(asm, alloc, a, dst_vr, FpBinKind::Sub, a.op.size_bits())?,
+        Op::Fmul32 | Op::Fmul64 => emit_fbinop(asm, alloc, a, dst_vr, FpBinKind::Mul, a.op.size_bits())?,
+        Op::Fdiv32 | Op::Fdiv64 => emit_fbinop(asm, alloc, a, dst_vr, FpBinKind::Div, a.op.size_bits())?,
 
         op if op.is_terminator() => {}
 
@@ -269,89 +246,86 @@ fn apply_bin_64(asm: &mut CodeAssembler, k: BinKind, l: AsmRegister64, r: AsmReg
     Ok(())
 }
 
-fn emit_binop_32(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, k: BinKind) -> Result<()> {
-    if let Some(d) = dst {
-        if let Loc::Reg(r) = alloc.loc(d) {
-            if alloc.loc(a.args[0]) == Loc::Reg(r) {
-                load32(asm, alloc, a.args[1], gpr32(scratch1_id()))?;
-                apply_bin_32(asm, k, gpr32(r), gpr32(scratch1_id()))?;
-                return Ok(());
+fn emit_binop(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, k: BinKind, bits: u32) -> Result<()> {
+    if bits == 64 {
+        if let Some(d) = dst {
+            if let Loc::Reg(r) = alloc.loc(d) {
+                if alloc.loc(a.args[0]) == Loc::Reg(r) {
+                    load64(asm, alloc, a.args[1], SCRATCH1)?;
+                    apply_bin_64(asm, k, gpr64(r), SCRATCH1)?;
+                    return Ok(());
+                }
             }
         }
-    }
-    load32(asm, alloc, a.args[0], eax)?;
-    load32(asm, alloc, a.args[1], gpr32(scratch1_id()))?;
-    apply_bin_32(asm, k, eax, gpr32(scratch1_id()))?;
-    if let Some(d) = dst { store32(asm, alloc, d, eax)?; }
-    Ok(())
-}
-
-fn emit_binop_64(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, k: BinKind) -> Result<()> {
-    if let Some(d) = dst {
-        if let Loc::Reg(r) = alloc.loc(d) {
-            if alloc.loc(a.args[0]) == Loc::Reg(r) {
-                load64(asm, alloc, a.args[1], SCRATCH1)?;
-                apply_bin_64(asm, k, gpr64(r), SCRATCH1)?;
-                return Ok(());
+        load64(asm, alloc, a.args[0], SCRATCH0)?;
+        load64(asm, alloc, a.args[1], SCRATCH1)?;
+        apply_bin_64(asm, k, SCRATCH0, SCRATCH1)?;
+        if let Some(d) = dst { store64(asm, alloc, d, SCRATCH0)?; }
+    } else {
+        if let Some(d) = dst {
+            if let Loc::Reg(r) = alloc.loc(d) {
+                if alloc.loc(a.args[0]) == Loc::Reg(r) {
+                    load32(asm, alloc, a.args[1], gpr32(scratch1_id()))?;
+                    apply_bin_32(asm, k, gpr32(r), gpr32(scratch1_id()))?;
+                    return Ok(());
+                }
             }
         }
+        load32(asm, alloc, a.args[0], eax)?;
+        load32(asm, alloc, a.args[1], gpr32(scratch1_id()))?;
+        apply_bin_32(asm, k, eax, gpr32(scratch1_id()))?;
+        if let Some(d) = dst { store32(asm, alloc, d, eax)?; }
     }
-    load64(asm, alloc, a.args[0], SCRATCH0)?;
-    load64(asm, alloc, a.args[1], SCRATCH1)?;
-    apply_bin_64(asm, k, SCRATCH0, SCRATCH1)?;
-    if let Some(d) = dst { store64(asm, alloc, d, SCRATCH0)?; }
     Ok(())
 }
 
 #[derive(Clone, Copy)]
 enum UnopKind { Not, Neg }
 
-fn emit_unop_32(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, k: UnopKind) -> Result<()> {
-    load32(asm, alloc, a.args[0], eax)?;
-    match k {
-        UnopKind::Not => asm.not(eax)?,
-        UnopKind::Neg => asm.neg(eax)?,
+fn emit_unop(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, k: UnopKind, bits: u32) -> Result<()> {
+    if bits == 64 {
+        load64(asm, alloc, a.args[0], SCRATCH0)?;
+        match k {
+            UnopKind::Not => asm.not(SCRATCH0)?,
+            UnopKind::Neg => asm.neg(SCRATCH0)?,
+        }
+        if let Some(d) = dst { store64(asm, alloc, d, SCRATCH0)?; }
+    } else {
+        load32(asm, alloc, a.args[0], eax)?;
+        match k {
+            UnopKind::Not => asm.not(eax)?,
+            UnopKind::Neg => asm.neg(eax)?,
+        }
+        if let Some(d) = dst { store32(asm, alloc, d, eax)?; }
     }
-    if let Some(d) = dst { store32(asm, alloc, d, eax)?; }
-    Ok(())
-}
-
-fn emit_unop_64(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, k: UnopKind) -> Result<()> {
-    load64(asm, alloc, a.args[0], SCRATCH0)?;
-    match k {
-        UnopKind::Not => asm.not(SCRATCH0)?,
-        UnopKind::Neg => asm.neg(SCRATCH0)?,
-    }
-    if let Some(d) = dst { store64(asm, alloc, d, SCRATCH0)?; }
     Ok(())
 }
 
 #[derive(Clone, Copy)]
 enum ShiftKind { Lsl, Lsr, Asr, Ror }
 
-fn emit_shift_32(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, kind: ShiftKind) -> Result<()> {
-    load32(asm, alloc, a.args[0], eax)?;
-    load32(asm, alloc, a.args[1], ecx)?;
-    match kind {
-        ShiftKind::Lsl => asm.shl(eax, cl)?,
-        ShiftKind::Lsr => asm.shr(eax, cl)?,
-        ShiftKind::Asr => asm.sar(eax, cl)?,
-        ShiftKind::Ror => asm.ror(eax, cl)?,
+fn emit_shift(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, kind: ShiftKind, bits: u32) -> Result<()> {
+    if bits == 64 {
+        load64(asm, alloc, a.args[0], SCRATCH0)?;
+        load64(asm, alloc, a.args[1], rcx)?;
+        match kind {
+            ShiftKind::Lsl => asm.shl(SCRATCH0, cl)?,
+            ShiftKind::Lsr => asm.shr(SCRATCH0, cl)?,
+            ShiftKind::Asr => asm.sar(SCRATCH0, cl)?,
+            ShiftKind::Ror => asm.ror(SCRATCH0, cl)?,
+        }
+        if let Some(d) = dst { store64(asm, alloc, d, SCRATCH0)?; }
+    } else {
+        load32(asm, alloc, a.args[0], eax)?;
+        load32(asm, alloc, a.args[1], ecx)?;
+        match kind {
+            ShiftKind::Lsl => asm.shl(eax, cl)?,
+            ShiftKind::Lsr => asm.shr(eax, cl)?,
+            ShiftKind::Asr => asm.sar(eax, cl)?,
+            ShiftKind::Ror => asm.ror(eax, cl)?,
+        }
+        if let Some(d) = dst { store32(asm, alloc, d, eax)?; }
     }
-    if let Some(d) = dst { store32(asm, alloc, d, eax)?; }
-    Ok(())
-}
-
-fn emit_shift_64(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, kind: ShiftKind) -> Result<()> {
-    load64(asm, alloc, a.args[0], SCRATCH0)?;
-    load64(asm, alloc, a.args[1], rcx)?;
-    match kind {
-        ShiftKind::Lsl => asm.shl(SCRATCH0, cl)?,
-        ShiftKind::Lsr => asm.shr(SCRATCH0, cl)?,
-        ShiftKind::Asr => asm.sar(SCRATCH0, cl)?,
-        ShiftKind::Ror => asm.ror(SCRATCH0, cl)?,
-    }
-    if let Some(d) = dst { store64(asm, alloc, d, SCRATCH0)?; }
     Ok(())
 }
 
@@ -398,11 +372,11 @@ fn emit_adc_sbc(
     a: Armlet,
     dst: Option<ValueRef>,
     is_sub: bool,
-    is_64: bool,
+    bits: u32,
 ) -> Result<()> {
     asm.bt(dword_ptr(CTX_REG + cpu_offsets::nzcv() as i32), 1i32)?;
     if is_sub { asm.cmc()?; }
-    if is_64 {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         load64(asm, alloc, a.args[1], rcx)?;
         if is_sub { asm.sbb(rax, rcx)?; } else { asm.adc(rax, rcx)?; }
@@ -422,12 +396,12 @@ fn emit_div(
     a: Armlet,
     dst: Option<ValueRef>,
     signed: bool,
-    is_64: bool,
+    bits: u32,
 ) -> Result<()> {
     let mut lbl_zero = asm.create_label();
     let mut lbl_done = asm.create_label();
 
-    if is_64 {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         load64(asm, alloc, a.args[1], rcx)?;
         asm.test(rcx, rcx)?;
@@ -478,10 +452,10 @@ fn emit_div(
     Ok(())
 }
 
-fn emit_clz(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, is_64: bool) -> Result<()> {
+fn emit_clz(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, bits: u32) -> Result<()> {
     let mut lbl_zero = asm.create_label();
     let mut lbl_done = asm.create_label();
-    if is_64 {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         asm.test(rax, rax)?;
         asm.jz(lbl_zero)?;
@@ -509,10 +483,10 @@ fn emit_clz(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<
     Ok(())
 }
 
-fn emit_cls(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, is_64: bool) -> Result<()> {
+fn emit_cls(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, bits: u32) -> Result<()> {
     let mut lbl_all_same = asm.create_label();
     let mut lbl_done = asm.create_label();
-    if is_64 {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         asm.mov(rcx, rax)?;
         asm.shl(rax, 1i32)?;
@@ -546,8 +520,8 @@ fn emit_cls(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<
     Ok(())
 }
 
-fn emit_rbit(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, is_64: bool) -> Result<()> {
-    if is_64 {
+fn emit_rbit(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, bits: u32) -> Result<()> {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         rbit64_inplace(asm)?;
         if let Some(d) = dst { store64(asm, alloc, d, rax)?; }
@@ -608,8 +582,8 @@ fn rbit32_inplace(asm: &mut CodeAssembler) -> Result<()> {
     Ok(())
 }
 
-fn emit_rev16(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, is_64: bool) -> Result<()> {
-    if is_64 {
+fn emit_rev16(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, bits: u32) -> Result<()> {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         asm.mov(rcx, rax)?;
         asm.shr(rcx, 8i32)?;
@@ -640,8 +614,8 @@ fn emit_rev32_within64(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, d
     Ok(())
 }
 
-fn emit_bswap(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, is_64: bool) -> Result<()> {
-    if is_64 {
+fn emit_bswap(asm: &mut CodeAssembler, alloc: &Allocation, a: Armlet, dst: Option<ValueRef>, bits: u32) -> Result<()> {
+    if bits == 64 {
         load64(asm, alloc, a.args[0], rax)?;
         asm.bswap(rax)?;
         if let Some(d) = dst { store64(asm, alloc, d, rax)?; }
@@ -819,41 +793,35 @@ fn scratch3_id() -> u8 {
 #[derive(Clone, Copy)]
 enum FpBinKind { Add, Sub, Mul, Div }
 
-fn emit_fbinop_s(
+fn emit_fbinop(
     asm: &mut CodeAssembler,
     alloc: &Allocation,
     a: Armlet,
     dst: Option<ValueRef>,
     k: FpBinKind,
+    bits: u32,
 ) -> Result<()> {
-    load_xmm_s(asm, alloc, a.args[0], xmm0)?;
-    load_xmm_s(asm, alloc, a.args[1], xmm1)?;
-    match k {
-        FpBinKind::Add => asm.addss(xmm0, xmm1)?,
-        FpBinKind::Sub => asm.subss(xmm0, xmm1)?,
-        FpBinKind::Mul => asm.mulss(xmm0, xmm1)?,
-        FpBinKind::Div => asm.divss(xmm0, xmm1)?,
+    if bits == 64 {
+        load_xmm_d(asm, alloc, a.args[0], xmm0)?;
+        load_xmm_d(asm, alloc, a.args[1], xmm1)?;
+        match k {
+            FpBinKind::Add => asm.addsd(xmm0, xmm1)?,
+            FpBinKind::Sub => asm.subsd(xmm0, xmm1)?,
+            FpBinKind::Mul => asm.mulsd(xmm0, xmm1)?,
+            FpBinKind::Div => asm.divsd(xmm0, xmm1)?,
+        }
+        if let Some(d) = dst { store_xmm_d(asm, alloc, d, xmm0)?; }
+    } else {
+        load_xmm_s(asm, alloc, a.args[0], xmm0)?;
+        load_xmm_s(asm, alloc, a.args[1], xmm1)?;
+        match k {
+            FpBinKind::Add => asm.addss(xmm0, xmm1)?,
+            FpBinKind::Sub => asm.subss(xmm0, xmm1)?,
+            FpBinKind::Mul => asm.mulss(xmm0, xmm1)?,
+            FpBinKind::Div => asm.divss(xmm0, xmm1)?,
+        }
+        if let Some(d) = dst { store_xmm_s(asm, alloc, d, xmm0)?; }
     }
-    if let Some(d) = dst { store_xmm_s(asm, alloc, d, xmm0)?; }
-    Ok(())
-}
-
-fn emit_fbinop_d(
-    asm: &mut CodeAssembler,
-    alloc: &Allocation,
-    a: Armlet,
-    dst: Option<ValueRef>,
-    k: FpBinKind,
-) -> Result<()> {
-    load_xmm_d(asm, alloc, a.args[0], xmm0)?;
-    load_xmm_d(asm, alloc, a.args[1], xmm1)?;
-    match k {
-        FpBinKind::Add => asm.addsd(xmm0, xmm1)?,
-        FpBinKind::Sub => asm.subsd(xmm0, xmm1)?,
-        FpBinKind::Mul => asm.mulsd(xmm0, xmm1)?,
-        FpBinKind::Div => asm.divsd(xmm0, xmm1)?,
-    }
-    if let Some(d) = dst { store_xmm_d(asm, alloc, d, xmm0)?; }
     Ok(())
 }
 
